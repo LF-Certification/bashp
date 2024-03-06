f9daba34169f8cc2ffb9bec0a728e3ade178cbc9
Daniel Ruthardt <druthardt@linuxfoundation.org>
2024-03-06 23:03:47 +0100

    🎉 Add initial version of Bash+ and an example
    Signed-off-by: Daniel Ruthardt <druthardt@linuxfoundation.org>

---
+++ hello-world.sh
@@ -0,0 +1,13 @@
+#!./bashp
+# shellcheck shell=bash
+
+# Option 1
+# Include (once) using the "#include" directive
+
+#include test/hello
+test::hello
+
+# Option 2
+# Rely on auto-include based on naming-convention "<package>::<function>"
+
+test::hello_world
